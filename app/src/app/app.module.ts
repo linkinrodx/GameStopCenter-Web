245d174cd6d7a9d7982d7e4c4cfe1d833de44ac9
Rodrigo <rodrigo.saraya@newmarketinc.com>
2022-05-17 07:34:01 -0500

    add menu bar and update header

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuBarComponent } from './shared/components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
