5ee5af666f98e986212f5a372cf7ca4fad742bd1
Rodrigo <rodrigo.saraya@newmarketinc.com>
2022-05-17 22:04:22 -0500

    add product view

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuBarComponent } from './shared/components/menu-bar/menu-bar.component';
+import { ProductViewComponent } from './components/product/product-view/product-view.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    ProductViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
